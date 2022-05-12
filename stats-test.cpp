a97d3f5acb4f1fa3929ea1a7aa429ad403758743
Shrishti Sharma <Shrishti211196@gmail.com>
2022-05-12 12:47:58 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -14,11 +14,17 @@ TEST_CASE("reports average, minimum and maximum") {
 }
 
 TEST_CASE("average is NaN for empty array") {
-    auto computedStats = Statistics::ComputeStatistics({});
+    auto set[] = {1.5, 8.9, 3.2, 4.5};
+    auto len = sizeof(set)/sizeof(set[0]);
+    auto computedStats = Statistics::ComputeStatistics(set,len);
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     
     //Design the REQUIRE statement here.
+    REQUIRE(isnan(computedStats.average))==0;
+    REQUIRE(isnan(computedStats.min))==0;
+    REQUIRE(isnan(computedStats.max))==0;
+    
     //Use http://www.cplusplus.com/reference/cmath/isnan/
 }
 
